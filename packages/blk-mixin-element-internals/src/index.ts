8c13ce998c9445a7dab5913b9dea454ec61665a2
oscar marina <oscar7600@outlook.com>
2025-07-30 15:30:05 +0200

    feat: create BlkMixinElementInternals as simple internals wrapper
    Provides clean internals getter without exposing internal symbol,
    built on top of BlkMixinInternalsBase for automatic initialization

--- packages/blk-mixin-element-internals/src/index.ts
+++ packages/blk-mixin-element-internals/src/index.ts
@@ -1,3 +1,4 @@
-export {BlkMixinElementInternals, internals} from './BlkMixinElementInternals.js';
+export {BlkMixinElementInternals} from './BlkMixinElementInternals.js';
+export {BlkMixinInternalsBase, internals} from './BlkMixinInternalsBase.js';
 export {BlkMixinFormAssociated, type FormAssociated} from './BlkMixinFormAssociated.js';
 export {BlkFormValidationEvent} from './BlkFormValidationEvent.js';
